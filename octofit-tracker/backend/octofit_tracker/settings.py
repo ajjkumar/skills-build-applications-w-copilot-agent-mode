ba00f3e2acb41c0284dd4b0f182e2ea971518905
ajjkumar <a.jj.kumar@accenture.com>
2026-05-04 10:36:09 +0000

    Update settings and URLs for CODESPACE_NAME support and API root response

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = 'django-insecure-#be#ew%(x26&ay*&ciy6=h5w5v+e4%&p4h+d4$^%rqzae!rz$t
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
